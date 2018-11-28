44b8fd4b7b0faed7163b07438951decdd0f01daa
Ali Mihandoost <alimihandoost@gmail.com>
2018-11-28 14:02:23 +0330

    fix: cleanup

--- woocommerce2.php
+++
@@ -1,7 +0,0 @@
-<?php get_header(); ?>
-    <section id="content-wrap" class="section woocommerce">
-        <div class="section-inner">
-            <?php woocommerce_content(); ?>
-        </div>
-    </section>
-<?php get_footer(); ?>
