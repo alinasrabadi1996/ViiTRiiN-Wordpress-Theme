6468c218ce9b3064eaaccee7f7748010ce6bb7a0
Ali Nasrabadi <info.ali.nasr@gmail.com>
2018-12-05 14:00:13 +0330

    Undo All Changes

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -54,11 +54,22 @@
 
 	<?php
 		$term = get_queried_object();
-		$image = site_url()."/wp-content/uploads/2018/03/categories-header.jpg";
-		if( get_field('intro_image', $term) && !empty(get_field('intro_image', $term)) )
-			$image = get_field('intro_image', $term)['url'];
+		$intro_image = get_field('intro_image', $term);
+		if(is_array($intro_image) && in_array('url', $intro_image) && !empty($intro_image['url']) ) {
+			$image = $intro_image['url'];
+		}
+		else {
+			$image = site_url()."/wp-content/uploads/2018/03/categories-header.jpg";
+		}
 	?>
-	<div style="background-image: url(<?php echo $image; ?>)" class="categories-header-slider <?php if($term_children) { echo "has-fixbar"; } ?>"></div>
+	<div style="background-image: url(<?php echo $image; ?>)" class="categories-header-slider <?php if($term_children) { echo "has-fixbar"; } ?>">
+		<!--
+		<div class="move-to-content-arrow" onclick="goto_products('.categories-products-section',58)">
+			<span class="slider-move-to-title">محصولات</span>
+			<i class="icon-keyboard_arrow_down"></i>
+		</div>
+		-->
+	</div>
 
 	<?php
 
