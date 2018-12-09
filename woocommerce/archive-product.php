ea5cd9a2e35de24ffb30565c8571b1aa3f295406
Ali Nasrabadi <info.ali.nasr@gmail.com>
2018-12-09 14:47:25 +0330

    Fix(Archive Products): Get Intro Img Value (For Test)

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -55,6 +55,9 @@
 	<?php
 		$term = get_queried_object();
 		$intro_image = get_field('intro_image', $term);
+
+		echo '<div style="display: none">'.$intro_image.'</div>';
+
 		if(is_array($intro_image) && in_array('url', $intro_image) && !empty($intro_image['url']) ) {
 			$image = $intro_image['url'];
 		}
