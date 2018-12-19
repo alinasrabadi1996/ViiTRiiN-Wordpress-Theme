4561a52d30c535c3484827a7cbeed97a87855ef3
Ali Nasrabadi <info.ali.nasr@gmail.com>
2018-12-19 12:44:55 +0330

    Fix(SMS): Handle Error From Soap

--- framework/api/sms/SendSMS_SoapClient.php
+++ framework/api/sms/SendSMS_SoapClient.php
@@ -2,17 +2,21 @@
 	//include_once("class.nusoap_base.php");
   	ini_set("soap.wsdl_cache_enabled", "0");
 	function SendSMS($text, $receiver) {
-		$client = new SoapClient('http://api.payamak-panel.com/post/send.asmx?wsdl', array('encoding'=>'UTF-8'));
-		$parameters['username'] = "viitriin";
-		$parameters['password'] = "kIUiodb8gZMQxp9pJNRN";
-		$parameters['from'] = "10007720";
-		$parameters['to'] = $receiver;
-		$parameters['text'] = $text;
-		$parameters['isflash'] = false;
+		try {
+			$client = new SoapClient('http://api.payamak-panel.com/post/send.asmx?wsdl', array('encoding'=>'UTF-8'));
+			$parameters['username'] = "viitriin";
+			$parameters['password'] = "kIUiodb8gZMQxp9pJNRN";
+			$parameters['from'] = "10007720";
+			$parameters['to'] = $receiver;
+			$parameters['text'] = $text;
+			$parameters['isflash'] = false;
 
-		if ($client->SendSimpleSMS2($parameters)->SendSimpleSMS2Result)
-			return 1;   
-		else 
-			return 0;
+			if ($client->SendSimpleSMS2($parameters)->SendSimpleSMS2Result)
+				return 1;   
+			else 
+				return 0;
+		} catch (SoapFault $e){
+			print_r($client);
+		}
 	}
 ?>
