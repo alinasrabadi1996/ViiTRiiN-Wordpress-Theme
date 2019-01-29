47144e549bc6c5d606953c150b0da2fc10b2000d
Ali Nasrabadi <info.ali.nasr@gmail.com>
2019-01-29 17:50:11 +0330

    Chore(Functions): Enable Tehran-Tehran Notice In Checkout Page

--- functions.php
+++ functions.php
@@ -127,10 +127,10 @@ https://viitriin.com";
     }
 }
 
-// add_action('woocommerce_checkout_before_order_review', 'display_payment_notice');
-// function display_payment_notice() {
-//     echo '<div id="payment_notice" style="display: none;background: #d22929;padding: 9px;color: #FFF;border-radius: 3px;margin-top: 3em;font-weight: bold;box-shadow: 0 0px 2px 0px rgba(0, 0, 0, 0.21176470588235294);font-size: 14px;border: 3px solid #bd3421;">در صورت انتخاب پرداخت آنلاین، سفارش شما در اولویت ارسال قرار گرفته و به صورت پیشتاز پست خواهد شد.</div>';
-// }
+add_action('woocommerce_checkout_before_order_review', 'display_payment_notice');
+function display_payment_notice() {
+    echo '<div id="payment_notice" style="display: none;background: #d22929;padding: 9px;color: #FFF;border-radius: 3px;margin-top: 3em;font-weight: bold;box-shadow: 0 0px 2px 0px rgba(0, 0, 0, 0.21176470588235294);font-size: 14px;border: 3px solid #bd3421;">در صورت انتخاب پرداخت آنلاین، سفارش شما در اولویت ارسال قرار گرفته و به صورت پیشتاز پست خواهد شد.</div>';
+}
 
 
 // add_action('woocommerce_after_cart_table', 'check_product_in_cart');
