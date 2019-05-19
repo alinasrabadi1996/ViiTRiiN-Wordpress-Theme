b5f22908e4d373b35a97b1abdeb3c3f583d53774
Ali Nasrabadi <info.ali.nasr@gmail.com>
2019-05-19 12:47:16 +0430

    Chore: Remove 'Google Tag Manager' & 'Google Analytics'

--- header.php
+++ header.php
@@ -13,15 +13,6 @@
     <script>
         var WRAjaxURL = '<?php echo esc_js( admin_url( 'admin-ajax.php' ) ); ?>';
     </script>
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-118341117-1"></script>
-    <script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-    
-      gtag('config', 'UA-118341117-1');
-    </script>
 </head>
 
 <?php
