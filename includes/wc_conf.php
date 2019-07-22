f99cfba407b130e3e61b5176439a00fcba8d1b85
Ali Nasrabadi <info.ali.nasr@gmail.com>
2019-07-22 16:29:31 +0430

    Chore(Chancekey): Change Countdown 24 To 72 Hours <Try 1>

--- includes/wc_conf.php
+++ includes/wc_conf.php
@@ -401,7 +401,7 @@ function new_coupon_type2($coupon_code, $discount, $national_code, $phone) {
     $result = $wpdb->get_results($query, ARRAY_A);
     if($result)
         foreach($result as $res)
-            if($res['date'] > date('Y-m-d H:i:s'))
+            if( date('Y-m-d H:i:s', strtotime($res['date'] . '+2 days')) > date('Y-m-d H:i:s') ) // +2 days = Expire Time(+1 days in db) + 48H == 72H
                 return array("sts" => 0, "coupon_code" => $res['coupon_code'], "coupon_expiration" => $res['date']);
     
     // Insert Fuckin Coupon
